6e8da6dd7418e449872c2ed36303e97d936f5f5e
Adrian Garza <adrianmgarza@gmail.com>
2017-10-27 08:37:10 -0400

    formats

--- scrape_wilshire.py
+++ scrape_wilshire.py
@@ -6,40 +6,45 @@ import re
 import time
 
 wilshires = []
+
+
 def get_wilshire_stocks(url):
-	check = True
-	try:
-		response = urllib2.urlopen(url)
-		html = response.read()
-	except urllib2.HTTPError as e:
-		print e.code  # pur a continue statement in the loop here to try the next date
-		check = False
-		time.sleep(1)
-	if check:
-		soup = BeautifulSoup(html)
-		for name in soup.find_all('tr'):
-			for index, x in enumerate(name):
-				if str(x) != ' ':
-					if index%2 == 0:
-						wilshires.append(str(x.contents[0].replace('\t', '')))
-				else:
-					pass
+    check = True
+    try:
+        response = urllib2.urlopen(url)
+        html = response.read()
+    except urllib2.HTTPError as e:
+        print e.code  # pur a continue statement in the loop here to try the next date
+        check = False
+        time.sleep(1)
+    if check:
+        soup = BeautifulSoup(html)
+        for name in soup.find_all('tr'):
+            for index, x in enumerate(name):
+                if str(x) != ' ':
+                    if index % 2 == 0:
+                        wilshires.append(str(x.contents[0].replace('\t', '')))
+                else:
+                    pass
+
 
 def prep_tickers(path):
-	def filter_(index, name):
-		if index%2 != 0:
-			return name
-		else:
-			pass
+    def filter_(index, name):
+        if index % 2 != 0:
+            return name
+        else:
+            pass
 
-	stocks = []
-	quads = []
-	with open(path, 'r') as f:
-		quads = [line.strip().split('\t') for line in f]
-		stocks = [filter_(index, name) for quad in quads for index, name in enumerate(quad)]
-	stocks = filter(lambda x: x is not None, stocks)
+    stocks = []
+    quads = []
+    with open(path, 'r') as f:
+        quads = [line.strip().split('\t') for line in f]
+        stocks = [filter_(index, name)
+                  for quad in quads for index, name in enumerate(quad)]
+    stocks = filter(lambda x: x is not None, stocks)
 
-	return stocks
+    return stocks
+
 
 url = 'http://financemainpage.com/Listing_of_All_Wilshire_5000_Stocks.html'
 get_wilshire_stocks(url)
@@ -49,5 +54,4 @@ get_wilshire_stocks(url)
 write_path = 'your_path/data/wilshire_stocks.txt'
 open(write_path, 'w').close()
 with open(write_path, 'r+') as g:
-	g.write(str(stocker))
-
+    g.write(str(stocker))
