56fd12e047377b56fc8c13b2e3d68103114b54a2
Adrian Garza <adrianmgarza@gmail.com>
2017-10-29 16:15:44 -0400

    cleans up

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-This code scraps the wilshire 5000 from http://financemainpage.com/Listing_of_All_Wilshire_5000_Stocks.html and prints them to a text files.  Examples of text files are stock_.txt and wilshire_stocks.txt.
+### Scrape Wilshire 5000
+This code scraps the wilshire 5000 from http://financemainpage.com/Listing_of_All_Wilshire_5000_Stocks.html and prints them to a text file.  An example of the text file printed is ./wilshire_stocks.txt
+
+###Example Usage:
+* python scrape_wilshire.py --OUTPUT_PATH ./wilshire_stocks.txt
--- scrape_wilshire.py
+++ scrape_wilshire.py
@@ -1,57 +1,70 @@
 
 from bs4 import BeautifulSoup
-import pprint
-import urllib2
-import re
+from urllib.error import URLError
+from urllib.request import urlopen
 import time
-
-wilshires = []
-
-
-def get_wilshire_stocks(url):
-    check = True
-    try:
-        response = urllib2.urlopen(url)
-        html = response.read()
-    except urllib2.HTTPError as e:
-        print e.code  # pur a continue statement in the loop here to try the next date
-        check = False
-        time.sleep(1)
-    if check:
-        soup = BeautifulSoup(html)
-        for name in soup.find_all('tr'):
-            for index, x in enumerate(name):
-                if str(x) != ' ':
-                    if index % 2 == 0:
-                        wilshires.append(str(x.contents[0].replace('\t', '')))
-                else:
-                    pass
+import argparse
 
 
-def prep_tickers(path):
-    def filter_(index, name):
-        if index % 2 != 0:
-            return name
-        else:
-            pass
+class Wilshire_Stocks(object):
 
-    stocks = []
-    quads = []
-    with open(path, 'r') as f:
-        quads = [line.strip().split('\t') for line in f]
-        stocks = [filter_(index, name)
-                  for quad in quads for index, name in enumerate(quad)]
-    stocks = filter(lambda x: x is not None, stocks)
+    def __init__(self, url, output_path):
+        self.wilshires = []
+        self.url = url
+        self.output_path = output_path
 
-    return stocks
+    def get_wilshire_stocks(self):
+        check = True
+        try:
+            response = urlopen(self.url)
+            html = response.read()
+        except URLError as e:
+            print(e.code)
+            check = False
+            time.sleep(1)
+        if check:
+            soup = BeautifulSoup(html, "lxml")
+            for name in soup.find_all('tr'):
+                for index, x in enumerate(name):
+                    if str(x) != ' ':
+                        if index % 2 == 0:
+                            self.wilshires.append(
+                                str(x.contents[0].replace('\t', '')))
+                    else:
+                        pass
+
+    def prep_tickers(self, path):
+        def filter_(index, name):
+            if index % 2 != 0:
+                return name
+            else:
+                pass
+
+        stocks = []
+        quads = []
+        with open(path, 'r') as f:
+            quads = [line.strip().split('\t') for line in f]
+            stocks = [filter_(index, name)
+                      for quad in quads for index, name in enumerate(quad)]
+        stocks = filter(lambda x: x is not None, stocks)
+
+        return stocks
+
+    def write_to_path(self, stocks):
+        open(self.output_path, 'w').close()
+        with open(self.output_path, 'r+') as g:
+            g.write(str(stocks))
 
 
-url = 'http://financemainpage.com/Listing_of_All_Wilshire_5000_Stocks.html'
-get_wilshire_stocks(url)
-# path = 'your_path/data/stock_.txt'
-# stocker = prep_tickers(path)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--OUTPUT_PATH',
+        required=True,
+        help='Please type a string for the path where to output (i.e. "Your_Path/write/" )')
 
-write_path = 'your_path/data/wilshire_stocks.txt'
-open(write_path, 'w').close()
-with open(write_path, 'r+') as g:
-    g.write(str(stocker))
+    args = parser.parse_args()
+    url = 'http://financemainpage.com/Listing_of_All_Wilshire_5000_Stocks.html'
+    ws = Wilshire_Stocks(url, args.OUTPUT_PATH)
+    ws.get_wilshire_stocks()
+    ws.write_to_path(ws.wilshires)
